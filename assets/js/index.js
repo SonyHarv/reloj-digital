b700a55ff79366c38f481c23cdbc8b43f33fa87f
Erikson TF <sonyharv9@gmail.com>
2021-09-27 22:25:36 -0700

    Add reloj function to display time in real time

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,13 +6,29 @@ function reloj() {
   let minutes = currentDate.getMinutes();
   let seconds = currentDate.getSeconds();
 
+  let beforeNoon = "AM";
+  if (hours === 0) {
+    hours = 12;
+  }
+  // let afterNoon= 'PM';
+  if (hours > 12) {
+    hours = hours - 12;
+    beforeNoon = "PM";
+  }
+
+  hours = hours < 10 ? "0" + hours : hours;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+
   // console.log(hours, minutes, seconds);
-  let time = `${hours} : ${minutes} : ${seconds}`;
+  let time = `${hours} : ${minutes} : ${seconds} ${beforeNoon}`;
 
   $(".reloj").innerText = time;
   $(".reloj").textContext = time;
 
-  setTimeout(reloj, 500);
+  // setTimeout(reloj, 1000);
 }
 
-reloj();
+window.onload = function () {
+  setInterval(reloj, 1000);
+};
