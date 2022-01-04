b72b574f991ca391f0e321860de3d3d1c7df855a
Erikson TF <sonyharv9@gmail.com>
2022-01-04 09:22:59 -0800

    feat: Add function to get time

--- main.js
+++ main.js
@@ -1,6 +1,39 @@
 import "./src/css/style.css";
+import { $ } from "./src/helpers/selector.js";
 
-document.querySelector("#app").innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`;
+function reloj() {
+  let currentDate = new Date();
+  let hours = currentDate.getHours();
+  let minutes = currentDate.getMinutes();
+  let seconds = currentDate.getSeconds();
+
+  let beforeNoon = "AM";
+  if (hours === 0) {
+    hours = 12;
+  }
+  // let afterNoon= 'PM';
+  if (hours > 12) {
+    hours = hours - 12;
+    beforeNoon = "PM";
+  }
+
+  hours = hours < 10 ? "0" + hours : hours;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+
+  // console.log(hours, minutes, seconds);
+  let time = `${hours} : ${minutes} : ${seconds} ${beforeNoon}`;
+
+  $(".reloj").innerText = time;
+  $(".reloj").textContext = time;
+
+  // setTimeout(reloj, 1000);
+}
+
+window.onload = function () {
+  setInterval(reloj, 1000);
+};
+
+//Alarma
+
+//function
